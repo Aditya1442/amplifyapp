4b40c711233335a3c25a92b7214c636fd766b2dd
Aditya1442 <aditya.thakor@triosstudent.com>
2023-04-17 00:02:45 -0400

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s32c8a8ae1": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
